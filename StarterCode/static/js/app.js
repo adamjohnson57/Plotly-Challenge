8309030cf203e78a81d98d646473abd4bc374cf5
Adam Johnson <adam.johnson57@gmail.com>
2021-01-16 17:19:43 -0600

    update app code

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -7,5 +7,17 @@ function  getPlot(id) {
 
     var wfreq = data.metadata.map(d => d.wfreq)
     console.log('Wash Freq: ${wfreq}')
-    }
+
+    // Filter sample values by ID
+    var samples = data.samples.filter(s => s.id.toString() === id)[0];
+    console.log(samples);
+
+    // Slice to get top 10 sample values and OTU IDs
+    var sampleValues = samples.sample_values.slice(0, 10).reverse();
+    var idValues = (samples.otu_ids.slice(0, 10)).reverse();
+
+    // Format OTU IDs for plot
+    var idOTU = idValues.map(d => "OTU" + d)
+    console.log('OTU IDs: ${idOTU}')
+}
 }
